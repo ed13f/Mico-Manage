b73de9a05d60375c445ff8c8d8fdec059b1ee8ee
Mark Siemers <mark.siemers@gmail.com>
2017-04-09 13:44:57 -0700

    Upgrade to ActiveRecord 5 - Update Gemfile, Gemfile.lock and models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
-class Event < ActiveRecord::Base
+class Event < ApplicationRecord
   validates :name, presence: { message: "An event must have a name." }
   validates :location, presence: { message: "An event must have a location." }
   validates :starts_at, presence: { message: "An event must have a properly formatted starting date and time." }
